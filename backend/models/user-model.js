90b5a60ccc41043fedf9ccbbdbfc560a46770deb
Sandra S <zandrastr@hotmail.com>
2023-03-24 15:52:14 +0100

    update user model with orders property

--- backend/models/user-model.js
+++ backend/models/user-model.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose");
 const UserSchema = mongoose.Schema({
     name: String,
     email: String,
-    password: String    
+    password: String,       
+    orders: {
+        type: [mongoose.Types.ObjectId],
+        ref: 'Order'
+    }
 })
 
 module.exports = mongoose.model("User", UserSchema);
