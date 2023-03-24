dd826f6d1705d82d2e613fa371212ff59f15ae7c
Sandra S <zandrastr@hotmail.com>
2023-03-24 15:49:35 +0100

    add routes to create and get orders

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,20 +1,40 @@
 var express = require('express');
 var router = express.Router();
+const OrderModel = require('../models/order-model');
+const UserModel = require('../models/user-model');
 
 // ****************************** ORDERS/ALL ******************************
 // HÄMTA ALLA ORDERS
-// GET
 
 /* GET orders listing. */
-router.get('/all', function(req, res, next) {
-    res.send('Orders listing.');
-});
+router.get('/all', async(req, res, next) => {
+    try {
+        const orders = await OrderModel.find()
+        res.status(200).json(orders)    
+    } 
+    catch(error) {
+        console.error(error, "Something went wrong, could not get orders listing.");
+    }
+})
 
 
 // ****************************** ORDERS/ADD ******************************
 // SKAPA ORDER FÖR EN SPECIFIK USER 
 // PRODUCTS ÄR EN ARRAY MOTSVARANDE INNEHÅLLET I KUNDVAGN
-// POST
 
+router.post('/add', async(req, res, next) => {
+    try {
+        const order = await OrderModel.create(req.body)
+
+        //find user
+        //add orderId to user.orders
+        const foundUser = await UserModel.findByIdAndUpdate(req.body.user, {$push:{orders: order._id}}, {new: true})
+        
+        res.status(201).json(order)
+    } 
+    catch(error) {
+        console.error(error, "Something went wrong, could not create order.");
+    }
+})
 
 module.exports = router;
