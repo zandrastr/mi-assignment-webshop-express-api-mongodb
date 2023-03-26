99a664364a73af1a3be5e45ffb4dc4ea1d3093db
Sandra S <zandrastr@hotmail.com>
2023-03-26 14:01:36 +0200

    update route to create order with lager amount

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const OrderModel = require('../models/order-model');
+const ProductModel = require('../models/product-model');
 const UserModel = require('../models/user-model');
 
 // ****************************** ORDERS/ALL ******************************
@@ -24,13 +25,25 @@ router.get('/all', async(req, res, next) => {
 
 router.post('/add', async(req, res, next) => {
     try {
-        const order = await OrderModel.create(req.body)
+        const {user, products} = req.body;
+        const createdOrder = await OrderModel.create(req.body)
 
         //find user
         //add orderId to user.orders
-        const foundUser = await UserModel.findByIdAndUpdate(req.body.user, {$push:{orders: order._id}}, {new: true})
+        const foundUser = await UserModel.findByIdAndUpdate(user, {$push:{orders: createdOrder._id}}, {new: true})
         
-        res.status(201).json(order)
+        //loop to update each product in order
+        products.forEach(async item => {
+
+            //find product
+            const foundProduct = await ProductModel.findById(item.productId)
+            const productId = foundProduct._id;
+
+            //update lager property in product
+            const currentLagerAmount = foundProduct.lager;
+            const updatedProduct = await ProductModel.findByIdAndUpdate(productId, {$set:{lager: (currentLagerAmount - item.quantity)}}, {new: true} )            
+        })
+        res.status(201).json(createdOrder)
     } 
     catch(error) {
         console.error(error, "Something went wrong, could not create order.");
